8dd7698f4f84a55ffc5b0286a58aa5f4b8d5af8e
Douglas <douglasantana007@gmail.com>
2021-11-02 23:18:20 -0200

    [WIP] Unit tests and Readme.md
    Tests of Repository and Controller, readme and settings in application
    properties.

--- forum/src/test/java/br/com/springboot/forum/repository/CursoRepositoryTest.java
+++ forum/src/test/java/br/com/springboot/forum/repository/CursoRepositoryTest.java
@@ -1,23 +1,37 @@
 package br.com.springboot.forum.repository;
 
-import br.com.springboot.forum.modelo.Curso;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.springboot.forum.modelo.Curso;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class CursoRepositoryTest {
 
 	@Autowired
-	private CursoRepository cursoRepository;
+	private CursoRepository   cursoRepository;
+	@Autowired
+	private TestEntityManager em;
 
 	@Test
 	public void getCursoPeloNome() {
 		String nomeCurso = "HTML 5";
+		
+		Curso html5 = new Curso();
+		html5.setNome(nomeCurso);
+		html5.setCategoria("Programação");
+		em.persist(html5);
+
 		Curso curso = cursoRepository.findByNome(nomeCurso);
 
 		Assertions.assertNotNull(curso);
